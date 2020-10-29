19bd01b9ed73e97598a4c5b4e0fb578f228c765f
Felipe Varela <56314243+felipevarelacarvalho@users.noreply.github.com>
2020-10-28 21:25:20 -0500

    Test Bench for CLM

--- phaseB/Control Logic Module/CLM.bak.vhd
+++ phaseB/Control Logic Module/CLM.bak.vhd
@@ -59,48 +59,9 @@ begin
 	
 	s_DMemWr <= '1' when funct = "101011" else
 				'0';
-
-	with opcode select s_RegWr <= 
-			'1' when "001000", --addi
-			'1' when "000000", --add,addu,srl,sw,lw,slt,sltu,and,or,xor,srav,nor,sll,sra,sllv,srlv
-			'1' when "001001", --addiu
-			'1' when "001010", --slti 
-			'1' when "001011", --sltiu 
-			'1' when "001100", --andi 
-			'1' when "001101", --ori 
-			'1' when "001110", --xori 
-			'1' when "001111", --lui
-			'1' when "100011", --lw 
-			'0' when others;
-	--PROGRESS SO FAR:
-	--	srl: SAME PROBLEM AS SLL, LOOKS LIKE IT SHIFTS TOO MUCH
-	--	add: WORKS
-	--	addiu: WORKS
-	--	addu: WORKS
-	--	sw: Haven't tested but after issue with lw is resolved should work
-	--	addi: WORKS
-	--	sub: WORKS
-	--	subu: WORKS
-	--	slt: WORKS
-	--	slti: WORKS
-	--	sltiu: DOESN'T WORK, PROBLEM IS IN SIGN EXTENDER? ASK TA
-	--	sltu: DOESN'T WORK, PROBLEM IS IN SIGN EXTENDER? ASK TA
-	--	and: WORKS
-	--	andi: WORKS
-	--	or: WORKS
-	--	ori: WORKS
-	--	xor: WORKS
-	--	xori: WORKS
-	--	srav: PROBLEM, SHIFTER MIGHT BE IN HEX? Maybe swap with Moe's shifter
-	--	nor: WORKS
-	--	lui: PROBLEM, NEED MUX CONNECTED TO 0 IN ALU? IF NOT PROBLEM IS IN ALU
-	--	lw: DOESN'T WORK, ASK TA!
-	--	sll: PROBLEM, SHIFTING TOO MUCH TO THE LEFT
-	--	sra: PROBLEM, SHIFTER MIGHT BE IN HEX? Maybe swap with Moe's shifter
-	--	sllv: PROBLEM, SHIFTING TOO MUCH TO THE LEFT
-	--	srlv: SAME AS SLL, LOOKS LIKE IT SHIFTS TOO MUCH
 	
-	--TAKEAWAYS: PROBLEMS IN SIGN EXTENDER(ASK TA), ALU(ASK TA), MAYBE SHIFTER?
+	s_RegWr <=  '0' when funct = "101011" else
+				'1';
 	
 	RegDst <=   '0' when opcode = "001001" else
 				'0' when opcode = "001000" else
