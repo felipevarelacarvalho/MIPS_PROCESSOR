89819a269ba220bcb1ca83c64617e4342b6c1d78
Rowland <trowland@iastate.edu>
2020-11-11 19:51:02 -0800

    Pipeline register templates made

--- Pipelined Processor/PipeLine Tests/ModelSimWork/src/ex_mem.vhd
+++ Pipelined Processor/PipeLine Tests/ModelSimWork/src/ex_mem.vhd
@@ -1,1 +1,38 @@
 --EX/MEM
+
+entity if_id is
+
+port(i_CLK 			: in std_logic; 						-- Clock Input
+	 i_RST_EXMEM	: in std_logic; 						-- Reset Input
+	 i_WE_EXMEM 	: in std_logic; 						-- Write Enable Input
+	 i_D_EXMEM 		: in std_logic_vector(N-1 downto 0); 	-- Data Value Input
+	 o_Q_EXMEM 		: out std_logic_vector(N-1 downto 0)); 	-- Data Value Output
+
+end if_id
+
+architecture structure of ex_mem is
+
+component Register_Nbits:
+
+  generic(N : integer :=32);
+  port(i_CLK        : in std_logic;     					 -- Clock input
+       i_RST        : in std_logic;     					 -- Reset input
+       i_WE         : in std_logic;     					 -- Write enable input
+       i_D          : in std_logic_vector(N-1 downto 0);     -- Data value input
+       o_Q          : out std_logic_vector(N-1 downto 0));   -- Data value output
+	   
+end Register_Nbits;
+
+--add any addt'l components
+
+--add logic here that's specific to IF/ID
+begin
+
+if_id_reg : Register_Nbits
+
+	port MAP(i_CLK <= i_CLK,
+		 i_RST_EXMEM <= i_RST,
+		 i_WE_EXMEM <= i_WE, 
+		 i_D_EXMEM <= i_D,
+		 o_Q_EXMEM <= o_Q
+	);
--- Pipelined Processor/PipeLine Tests/ModelSimWork/src/id_ex.vhd
+++ Pipelined Processor/PipeLine Tests/ModelSimWork/src/id_ex.vhd
@@ -1,1 +1,38 @@
 --ID/EX
+
+entity if_id is
+
+port(i_CLK 		: in std_logic; 						-- Clock Input
+	 i_RST_IDEX : in std_logic; 						-- Reset Input
+	 i_WE_IDEX 	: in std_logic; 						-- Write Enable Input
+	 i_D_IDEX 	: in std_logic_vector(N-1 downto 0); 	-- Data Value Input
+	 o_Q_IDEX 	: out std_logic_vector(N-1 downto 0)); 	-- Data Value Output
+
+end if_id
+
+architecture structure of id_ex is
+
+component Register_Nbits:
+
+  generic(N : integer :=32);
+  port(i_CLK        : in std_logic;     -- Clock input
+       i_RST        : in std_logic;     -- Reset input
+       i_WE         : in std_logic;     -- Write enable input
+       i_D          : in std_logic_vector(N-1 downto 0);     -- Data value input
+       o_Q          : out std_logic_vector(N-1 downto 0));   -- Data value output
+	   
+end Register_Nbits;
+
+--add any addt'l components
+
+--add logic here that's specific to IF/ID
+begin
+
+if_id_reg : Register_Nbits
+
+	port MAP(i_CLK <= i_CLK,
+		 i_RST_IFID <= i_RST,
+		 i_WE_IFID <= i_WE, 
+		 i_D_IFID <= i_D,
+		 o_Q_IFID <= o_Q
+	);
