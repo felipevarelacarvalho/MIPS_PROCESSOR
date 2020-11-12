d71526fbd69eb03f8da23cc12dffe02e8ad20afe
Felipe Varela <56314243+felipevarelacarvalho@users.noreply.github.com>
2020-11-12 08:53:16 -0600

    Updated IFID, IDEX pipeline registers

--- Pipelined Processor/PipeLine Tests/ModelSimWork/src/id_ex.vhd
+++ Pipelined Processor/PipeLine Tests/ModelSimWork/src/id_ex.vhd
@@ -1,12 +1,12 @@
 --ID/EX
+library IEEE;
+use IEEE.std_logic_1164.all;
 
 entity if_id is
-
 generic(N : integer := 32);
 port(i_CLK 				: in std_logic; 						-- Clock Input
 	 i_RST_IDEX 		: in std_logic; 						-- Reset Input
 	 i_WE_IDEX 			: in std_logic; 						-- Write Enable Input
-	 o_Q_IDEX 			: out std_logic_vector(N-1 downto 0); 	-- Data Value Output
 	 --These are for just ID/EX ------------------------------------------------------
 	 i_JAL_IDEX 		: in std_logic;							-- Jal Instruction
 	 i_J_IDEX 			: in std_logic;							-- J Instruction
@@ -16,12 +16,27 @@ port(i_CLK 				: in std_logic; 						-- Clock Input
 	 i_MEMWR_IDEX		: in std_logic;							-- MemWrite Instruction
 	 i_ALUOP_IDEX		: in std_logic_vector(3 downto 0);		-- ALU_OP Instruction
 	 i_ALUSRC_IDEX		: in std_logic;							-- ALU_Src Instruction
+	 i_BRANCH_ADDR_IDEX : in std_logic_vector(N-1 downto 0);    --Branch Addr.
 	 i_JUMP_ADDR_IDEX 	: in std_logic_vector(N-1 downto 0);	-- Jump Addr.
 	 i_SIGN_EXT_IDEX	: in std_logic_vector(N-1 downto 0);	-- PC+4 and SignExtend
 	 i_READ_DATA1_IDEX	: in std_logic_vector(N-1 downto 0);	-- Read Data 1
 	 i_READ_DATA2_IDEX	: in std_logic_vector(N-1 downto 0);	-- Read Data 2
 	 i_PC_PLUS_4_IDEX 	: in std_logic_vector(N-1 downto 0);	-- PC+4
 	 ---------------------------------------------------------------------------------
+	 o_JAL_IDEX 		: in std_logic;							-- Jal Instruction
+	 o_J_IDEX 			: in std_logic;							-- J Instruction
+	 o_BNE_IDEX			: in std_logic;							-- Bne Instruction
+	 o_BEQ_IDEX			: in std_logic;							-- Beq Instruction
+	 o_MEMTOREG_IDEX	: in std_logic;							-- MemToReg Instruction
+	 o_MEMWR_IDEX		: in std_logic;							-- MemWrite Instruction
+	 o_ALUOP_IDEX		: in std_logic_vector(3 downto 0);		-- ALU_OP Instruction
+	 o_ALUSRC_IDEX		: in std_logic;							-- ALU_Src Instruction
+	 o_BRANCH_ADDR_IDEX : in std_logic_vector(N-1 downto 0);    --Branch Addr.
+	 o_JUMP_ADDR_IDEX 	: in std_logic_vector(N-1 downto 0);	-- Jump Addr.
+	 o_SIGN_EXT_IDEX	: in std_logic_vector(N-1 downto 0);	-- PC+4 and SignExtend
+	 o_READ_DATA1_IDEX	: in std_logic_vector(N-1 downto 0);	-- Read Data 1
+	 o_READ_DATA2_IDEX	: in std_logic_vector(N-1 downto 0);	-- Read Data 2
+	 o_PC_PLUS_4_IDEX 	: in std_logic_vector(N-1 downto 0);	-- PC+4
 
 end if_id
 
@@ -36,28 +51,28 @@ component Register_Nbits:
        i_D		: in std_logic_vector(N-1 downto 0);     -- Data value input
        o_Q		: out std_logic_vector(N-1 downto 0));   -- Data value output
 	   
-end Register_Nbits;
+end component;
 
 --add any addt'l components
 
 -- 32 bit output signals
-signal s_readData1		: std_logic_vector(31 downto 0); -- READ_DATA1
-signal s_readData2		: std_logic_vector(31 downto 0); -- READ_DATA2
-signal s_jumpAddr		: std_logic_vector(31 downto 0); -- Jump Addr.
-signal s_signExt		: std_logic_vector(31 downto 0); --(PC+4) + SignExt
-signal s_pcPlus4		: std_logic_vector(31 downto 0); -- PC+4
+--signal s_readData1		: std_logic_vector(31 downto 0); -- READ_DATA1
+--signal s_readData2		: std_logic_vector(31 downto 0); -- READ_DATA2
+--signal s_jumpAddr		: std_logic_vector(31 downto 0); -- Jump Addr.
+--signal s_signExt		: std_logic_vector(31 downto 0); --(PC+4) + SignExt
+--signal s_pcPlus4		: std_logic_vector(31 downto 0); -- PC+4
 
 -- 4 bit output signal
-signal s_aluOp 			: std_logic_vector(3 downto 0); -- aluOp
+--signal s_aluOp 			: std_logic_vector(3 downto 0); -- aluOp
 
 -- 1 bit output signals
-signal s_jal 			: std_logic_vector(1 downto 0); -- jal
-signal s_j 				: std_logic_vector(1 downto 0); -- j
-signal s_bne 			: std_logic_vector(1 downto 0); -- bne
-signal s_beq 			: std_logic_vector(1 downto 0); -- beq
-signal s_memToReg 		: std_logic_vector(1 downto 0); -- memToReg
-signal s_memWr 			: std_logic_vector(1 downto 0); -- memWr
-signal s_aluSrc 		: std_logic_vector(1 downto 0); -- aluSrc
+--signal s_jal 			: std_logic_vector(1 downto 0); -- jal
+--signal s_j 				: std_logic_vector(1 downto 0); -- j
+--signal s_bne 			: std_logic_vector(1 downto 0); -- bne
+--signal s_beq 			: std_logic_vector(1 downto 0); -- beq
+--signal s_memToReg 		: std_logic_vector(1 downto 0); -- memToReg
+--signal s_memWr 			: std_logic_vector(1 downto 0); -- memWr
+--signal s_aluSrc 		: std_logic_vector(1 downto 0); -- aluSrc
 
 begin
 
@@ -70,7 +85,7 @@ id_ex_data_reg1 : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_READ_DATA1_IDEX,
-			 o_Q 	=> s_readData1
+			 o_Q 	=> o_READ_DATA1_IDEX
 	);
 	
 -- READ_DATA2
@@ -80,7 +95,7 @@ id_ex_data_reg2 : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_READ_DATA2_IDEX,
-			 o_Q 	=> s_readData2
+			 o_Q 	=> o_READ_DATA2_IDEX
 	);
 	
 --for each input make a specific n-bit register for it
@@ -92,9 +107,19 @@ id_ex_jumpAddr_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_JUMP_ADDR_IDEX,
-			 o_Q 	=> s_jumpAddr
+			 o_Q 	=> o_JUMP_ADDR_IDEX
 	);
 	
+-- Branch Addr.
+id_ex_branchAddr_reg : Register_Nbits
+	generic MAP(N => 32);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_IDEX,
+			 i_WE 	=> i_WE_IDEX, 
+			 i_D 	=> i_BRANCH_ADDR_IDEX,
+			 o_Q 	=> o_BRANCH_ADDR_IDE
+	);
+
 --(PC+4)+SignExt
 id_ex_PC4_SignExt_reg : Register_Nbits
 	generic MAP(N => 32);
@@ -102,7 +127,7 @@ id_ex_PC4_SignExt_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_SIGN_EXT_IDEX,
-			 o_Q 	=> s_signExt
+			 o_Q 	=> o_SIGN_EXT_IDEX
 	);
 	
 -- PC+4
@@ -112,7 +137,7 @@ id_ex_PC4_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_PC_PLUS_4_IDEX,
-			 o_Q 	=> s_pcPlus4
+			 o_Q 	=> o_PC_PLUS_4_IDEX
 	);
 	
 -- 4 Bit Register(ALUOp)
@@ -122,7 +147,7 @@ id_ex_ALUOp_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_ALUOP_IDEX,
-			 o_Q 	=> s_aluOp
+			 o_Q 	=> o_ALUOP_IDEX
 	);
 	
 -- 1 Bit Registers(jal, j, bne, beq, memToReg, memWr, ALUSrc)
@@ -134,7 +159,7 @@ id_ex_jal_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_JAL_IDEX,
-			 o_Q 	=> s_jal
+			 o_Q 	=> o_JAL_IDEX
 	);
 	
 -- j
@@ -144,7 +169,7 @@ id_ex_j_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_J_IDEX,
-			 o_Q 	=> s_j
+			 o_Q 	=> o_J_IDEX
 	);
 	
 -- bne
@@ -154,7 +179,7 @@ id_ex_bne_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_BNE_IDEX,
-			 o_Q 	=> s_bne
+			 o_Q 	=> o_BNE_IDEX
 	);
 	
 -- beq
@@ -164,7 +189,7 @@ id_ex_beq_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_BEQ_IDEX,
-			 o_Q 	=> s_beq
+			 o_Q 	=> o_BEQ_IDEX
 	);
 	
 -- memToReg
@@ -174,7 +199,7 @@ id_ex_memToReg_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_MEMTOREG_IDEX,
-			 o_Q 	=> s_memToReg
+			 o_Q 	=> o_MEMTOREG_IDEX
 	);
 	
 -- memWr
@@ -184,7 +209,7 @@ id_ex_memWr_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_MEMWR_IDEX,
-			 o_Q 	=> s_memWr
+			 o_Q 	=> o_MEMWR_IDEX
 	);
 	
 -- ALUSrc
@@ -194,6 +219,6 @@ id_ex_ALUSrc_reg : Register_Nbits
 			 i_RST 	=> i_RST_IDEX,
 			 i_WE 	=> i_WE_IDEX, 
 			 i_D 	=> i_ALUSRC_IDEX,
-			 o_Q 	=> s_aluSrc
+			 o_Q 	=> o_ALUSRC_IDEX
 	);
 	
--- Pipelined Processor/PipeLine Tests/ModelSimWork/src/if_id.vhd
+++ Pipelined Processor/PipeLine Tests/ModelSimWork/src/if_id.vhd
@@ -1,40 +1,41 @@
 --IF/ID
+library IEEE;
+use IEEE.std_logic_1164.all;
 
 entity if_id is
-
-generic(N : integer);
+generic(N : integer := 32);
 port(i_CLK 			: in std_logic; 						-- Clock Input
-	 i_RST_IFID 	: in std_logic; 						-- Reset Input
+	 i_RST_IFID 	: in std_logic; 						-- Reset Input (Can be used as the Flush input)
 	 i_WE_IFID 		: in std_logic; 						-- Write Enable Input
 	 -- these are specific to IF/ID --------------------------------------------------------
-	 i_FLUSH_IFID	: in std_logic;							-- Flush Enable(Comes from CLM)
+	 --i_FLUSH_IFID	: in std_logic;							-- Flush Enable(Comes from CLM)
 	 i_PCplusFOUR	: in std_logic_vector(N-1 downto 0);	-- PC+4(From PC)
 	 i_MEM_IFID		: in std_logic_vector(N-1 downto 0);	-- Instruction Memory(From IMEM)
 	 ---------------------------------------------------------------------------------------
-	 o_Q_IFID 		: out std_logic_vector(N-1 downto 0)); 	-- Data Value Output
-
-end if_id
+	 o_PCplusFOUR	: in std_logic_vector(N-1 downto 0);	-- PC+4(From PC)
+	 o_MEM_IFID		: in std_logic_vector(N-1 downto 0);	-- Instruction Memory(From IMEM)
+end if_id;
 
 architecture structure of if_id is
 
-component Register_Nbits:
-
+component Register_Nbits is
+  generic(N : integer);
   port(i_CLK        : in std_logic;     					 -- Clock input
        i_RST        : in std_logic;     					 -- Reset input
        i_WE         : in std_logic;     					 -- Write enable input
        i_D          : in std_logic_vector(N-1 downto 0);     -- Data value input
        o_Q          : out std_logic_vector(N-1 downto 0));   -- Data value output
 	   
-end Register_Nbits;
+end component;
 
 --add any addt'l components
 
 --32 bit signals for Instruction Memory and PC+4 Outputs
-signal s_iMem_IFID	: std_logic_vector(31 downto 0); -- Instruction Memory
-signal s_PC4_IFID	: std_logic_vector(31 downto 0); -- PC+4
+--signal s_iMem_IFID	: std_logic_vector(31 downto 0); -- Instruction Memory
+--signal s_PC4_IFID	: std_logic_vector(31 downto 0); -- PC+4
 
 --1 bit signal for the Flush
-signal s_flush_IFID		: std_logic; -- Flush
+--signal s_flush_IFID		: std_logic; -- Flush
 
 --add logic here that's specific to IF/ID
 --No hazard Detection
@@ -50,7 +51,7 @@ if_id_iMem_reg : Register_Nbits
 		 i_RST => i_RST_IFID,
 		 i_WE => i_WE_IFID, 
 		 i_D => i_MEM_IFID,
-		 o_Q => s_iMem_IFID
+		 o_Q => o_MEM_IFID
 	);
 	
 -- PC+4
@@ -60,17 +61,17 @@ if_id_pc4_reg : Register_Nbits
 			 i_RST => i_RST_IFID,
 			 i_WE => i_WE_IFID, 
 			 i_D => i_PCplusFOUR,
-			 o_Q => s_PC4_IFID
+			 o_Q => o_PCplusFOUR
 	);
 	
---1 bit register for Flush Signal
-if_id_Flush_Reg : Register_Nbits
-	generic MAP(N => 1);
-	port MAP(i_CLK => i_CLK,
-			 i_RST => i_RST_IFID,
-			 i_WE => i_WE_IFID, 
-			 i_D => i_FLUSH_IFID,
-			 o_Q => s_flush_IFID
-	);
+--1 bit register for Flush Signal (Not needed for this phase of the project)
+--if_id_Flush_Reg : Register_Nbits
+--	generic MAP(N => 1);
+--	port MAP(i_CLK => i_CLK,
+--			 i_RST => i_RST_IFID,
+--			 i_WE => i_WE_IFID, 
+--			 i_D => i_FLUSH_IFID,
+--			 o_Q => s_flush_IFID
+--	);
 	
 	
