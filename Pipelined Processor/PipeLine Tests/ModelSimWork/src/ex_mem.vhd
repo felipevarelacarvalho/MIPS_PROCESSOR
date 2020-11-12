5c66bafec0b77393b96f8f6b46bc114c5c836344
Stilic <mstilic@iastate.edu>
2020-11-12 09:25:49 -0600

    EXMEM and MEMWB

--- Pipelined Processor/PipeLine Tests/ModelSimWork/src/ex_mem.vhd
+++ Pipelined Processor/PipeLine Tests/ModelSimWork/src/ex_mem.vhd
@@ -1,39 +1,105 @@
 --EX/MEM
+-- By Trevor, Muhamed, Felipe
+library IEEE;
+use IEEE.std_logic_1164.all;
 
-entity if_id is
+entity ex_mem is
 
 generic(N : integer := 32);
-port(i_CLK 			: in std_logic; 						-- Clock Input
-	 i_RST_EXMEM	: in std_logic; 						-- Reset Input
-	 i_WE_EXMEM 	: in std_logic; 						-- Write Enable Input
-	 i_D_EXMEM 		: in std_logic_vector(N-1 downto 0); 	-- Data Value Input
-	 o_Q_EXMEM 		: out std_logic_vector(N-1 downto 0)); 	-- Data Value Output
-
-end if_id
+port(i_CLK 				: in std_logic; 						-- Clock Input
+	 i_RST_EXMEM		: in std_logic; 						-- Reset Input
+	 i_WE_EXMEM 		: in std_logic; 						-- Write Enable Input
+	 o_Q_EXMEM 			: out std_logic_vector(N-1 downto 0)); 	-- Data Value Output
+	 -- INPUT PORTS FOR EX-MEM
+	 i_JAL_EXMEM 		: in std_logic;							-- Jal Instruction
+	 i_MEMTOREG_EXMEM	: in std_logic;							-- MemToReg Instruction
+	 i_MEMWR_EXMEM		: in std_logic;							-- MemWrite Instruction
+	 i_PC_PLUS_4_EXMEM 	: in std_logic_vector(N-1 downto 0);	-- PC+4
+	 i_ALU_OUT_EXMEM	: in std_logic_vector(N-1 downto 0);	-- input fromt the ALU output
+	 i_READ_DATA2_EXMEM	: in std_logic_vector(N-1 downto 0);	-- Read Data 2
+	 -- OUTPUT PORTS FOR EX-MEM
+	 o_JAL_EXMEM 		: in std_logic;							-- Jal Instruction
+	 o_MEMTOREG_EXMEM	: in std_logic;							-- MemToReg Instruction
+	 o_MEMWR_EXMEM		: in std_logic;							-- MemWrite Instruction
+	 o_PC_PLUS_4_EXMEM 	: in std_logic_vector(N-1 downto 0);	-- PC+4
+	 o_ALU_OUT_EXMEM	: in std_logic_vector(N-1 downto 0);	-- input fromt the ALU output
+	 o_READ_DATA2_EXMEM	: in std_logic_vector(N-1 downto 0));	-- Read Data 2
+end ex_mem;
 
 architecture structure of ex_mem is
 
-component Register_Nbits:
-
+--Register(N-bits)
+component Register_Nbits is
   generic(N : integer := 32);
-  port(i_CLK        : in std_logic;     					 -- Clock input
-       i_RST        : in std_logic;     					 -- Reset input
-       i_WE         : in std_logic;     					 -- Write enable input
-       i_D          : in std_logic_vector(N-1 downto 0);     -- Data value input
-       o_Q          : out std_logic_vector(N-1 downto 0));   -- Data value output
-	   
-end Register_Nbits;
+  port(i_CLK	: in std_logic;     					 -- Clock input
+       i_RST	: in std_logic;     					 -- Reset input
+       i_WE		: in std_logic;     					 -- Write enable input
+       i_D		: in std_logic_vector(N-1 downto 0);     -- Data value input
+       o_Q		: out std_logic_vector(N-1 downto 0));   -- Data value output
+end component;
 
---add any addt'l components
+-- 32 Bit Registers(READ_DATA2,ALU-OUTPUT-INPUT,PC+4)
+begin 
 
---add logic here that's specific to IF/ID
-begin
-
-if_id_reg : Register_Nbits
-
-	port MAP(i_CLK <= i_CLK,
-		 i_RST_EXMEM <= i_RST,
-		 i_WE_EXMEM <= i_WE, 
-		 i_D_EXMEM <= i_D,
-		 o_Q_EXMEM <= o_Q
+-- READ_DATA2
+	ex_mem_data2 : Register_Nbits
+	generic MAP(N => 32);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_EXMEM,
+			 i_WE 	=> i_WE_EXMEM, 
+			 i_D 	=> i_READ_DATA2_EXMEM,
+			 o_Q 	=> o_READ_DATA2_EXMEM
 	);
+
+-- ALU_OUT_INPUT
+	ex_mem_alu_OUT_IN : Register_Nbits
+	generic MAP(N => 32);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_EXMEM,
+			 i_WE 	=> i_WE_EXMEM, 
+			 i_D 	=> i_ALU_OUT_EXMEM,
+			 o_Q 	=> o_ALU_OUT_EXMEM
+	);
+-- PC+4
+	ex_mem_PC4_reg : Register_Nbits
+	generic MAP(N => 32);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_EXMEM,
+			 i_WE 	=> i_WE_EXMEM, 
+			 i_D 	=> i_PC_PLUS_4_EXMEM,
+			 o_Q 	=> o_PC_PLUS_4_EXMEM
+	);
+	
+-- 1 Bit Registers(jal, memToReg, memWr)
+
+-- jal
+ex_mem_jal_reg : Register_Nbits
+	generic MAP(N => 1);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_EXMEM,
+			 i_WE 	=> i_WE_EXMEM, 
+			 i_D 	=> i_JAL_EXMEM,
+			 o_Q 	=> o_JAL_EXMEM
+	);
+	
+-- memToReg
+ex_mem_memToReg_reg : Register_Nbits
+	generic MAP(N => 1);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_EXMEM,
+			 i_WE 	=> i_WE_EXMEM, 
+			 i_D 	=> i_MEMTOREG_EXMEM,
+			 o_Q 	=> o_MEMTOREG_EXMEM
+	);
+	
+-- memWr
+ex_mem_memWr_reg : Register_Nbits
+	generic MAP(N => 1);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_EXMEM,
+			 i_WE 	=> i_WE_EXMEM, 
+			 i_D 	=> i_MEMWR_EXMEM,
+			 o_Q 	=> o_MEMWR_EXMEM
+	);
+	
+end structure;
--- Pipelined Processor/PipeLine Tests/ModelSimWork/src/mem_wb.vhd
+++ Pipelined Processor/PipeLine Tests/ModelSimWork/src/mem_wb.vhd
@@ -1,36 +1,94 @@
 --MEM/WB
+-- By Trevor, Muhamed, Felipe
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity mem_wb is
-
-port(i_CLK	: in std_logic;
-     i_RST_MEMWB: in std_logic;
-     i_WE_MEMWB : in std_logic;
-     i_D_MEMWB  : in std_logic_vector(N-1 downto 0);
-     o_Q_MEMWB  : out std_logic_vector(N-1 downto 0));
-
-end mem_web;
+generic(N : integer := 32);
+port(i_CLK 				: in std_logic; 						-- Clock Input
+	 i_RST_MEMWB		: in std_logic; 						-- Reset Input
+	 i_WE_MEMWB 		: in std_logic; 						-- Write Enable Input
+	 o_Q_MEMWB 			: out std_logic_vector(N-1 downto 0)); 	-- Data Value Output
+	 -- INPUT PORTS FOR MEM-WB
+	 i_JAL_MEMWB 		: in std_logic;							-- Jal Instruction
+	 i_MEMTOREG_MEMWB	: in std_logic;							-- MemToReg Instruction
+	 i_PC_PLUS_4_MEMWB 	: in std_logic_vector(N-1 downto 0);	-- PC+4
+	 i_MEMWR_READ_MEMWB	: in std_logic_vector(N-1 downto 0);	-- input fromt the ALU output
+	 i_ALU_OUT_MEMWB	: in std_logic_vector(N-1 downto 0);	-- Read Data 2
+	 -- OUTPUT PORTS FOR MEM-WB
+	 o_JAL_MEMWB 		: in std_logic;							-- Jal Instruction
+	 o_MEMTOREG_MEMWB	: in std_logic;							-- MemToReg Instruction
+	 o_PC_PLUS_4_MEMWB 	: in std_logic_vector(N-1 downto 0);	-- PC+4
+	 o_MEMWR_READ_MEMWB	: in std_logic_vector(N-1 downto 0);	-- input fromt the ALU output
+	 o_ALU_OUT_MEMWB	: in std_logic_vector(N-1 downto 0));	-- Read Data 2
+end mem_wb;
 
 architecture structure of mem_wb is
 
+--Register(N-bits)
 component Register_Nbits is
-  generic(N : integer :=32);
-  port(i_CLK        : in std_logic;     -- Clock input
-       i_RST        : in std_logic;     -- Reset input
-       i_WE         : in std_logic;     -- Write enable input
-       i_D          : in std_logic_vector(N-1 downto 0);     -- Data value input
-       o_Q          : out std_logic_vector(N-1 downto 0));   -- Data value output
+  generic(N : integer := 32);
+  port(i_CLK	: in std_logic;     					 -- Clock input
+       i_RST	: in std_logic;     					 -- Reset input
+       i_WE		: in std_logic;     					 -- Write enable input
+       i_D		: in std_logic_vector(N-1 downto 0);     -- Data value input
+       o_Q		: out std_logic_vector(N-1 downto 0));   -- Data value output
 end component;
 
-begin
-mem_wb_register: Register_Nbits
-port MAP(
-       i_CLK        => i_CLK,     -- Clock input
-       i_RST        => i_RST_MEMWB,     -- Reset input
-       i_WE         => i_WE_MEMWB,     -- Write enable input
-       i_D          => i_D_MEMWB,     -- Data value input
-       o_Q          => o_Q_MEMWB);    -- Data value output
+-- 32 Bit Registers(READ_DATA2,ALU-OUTPUT-INPUT,PC+4)
+begin 
+
+-- READ_DATA2
+	mem_wb_memwr_read_data : Register_Nbits
+	generic MAP(N => 32);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_MEMWB,
+			 i_WE 	=> i_WE_MEMWB, 
+			 i_D 	=> i_MEMWR_READ_MEMWB,
+			 o_Q 	=> o_MEMWR_READ_MEMWB
+	);
+
+-- ALU_OUT_INPUT
+	mem_wb_alu_OUT_IN : Register_Nbits
+	generic MAP(N => 32);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_MEMWB,
+			 i_WE 	=> i_WE_MEMWB, 
+			 i_D 	=> i_ALU_OUT_MEMWB,
+			 o_Q 	=> o_ALU_OUT_MEMWB
+	);
+-- PC+4
+	mem_wb_PC4_reg : Register_Nbits
+	generic MAP(N => 32);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_MEMWB,
+			 i_WE 	=> i_WE_MEMWB, 
+			 i_D 	=> i_PC_PLUS_4_MEMWB,
+			 o_Q 	=> o_PC_PLUS_4_MEMWB
+	);
+	
+-- 1 Bit Registers(jal, memToReg, memWr)
+
+-- jal
+mem_wb_jal_reg : Register_Nbits
+	generic MAP(N => 1);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_MEMWB,
+			 i_WE 	=> i_WE_MEMWB, 
+			 i_D 	=> i_JAL_MEMWB,
+			 o_Q 	=> o_JAL_MEMWB
+	);
+	
+-- memToReg
+mem_wb_memToReg_reg : Register_Nbits
+	generic MAP(N => 1);
+	port MAP(i_CLK 	=> i_CLK,
+			 i_RST 	=> i_RST_MEMWB,
+			 i_WE 	=> i_WE_MEMWB, 
+			 i_D 	=> i_MEMTOREG_MEMWB,
+			 o_Q 	=> o_MEMTOREG_MEMWB
+	);
+	
 end structure;
 
 	
